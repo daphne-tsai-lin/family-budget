89fc5f24d8514a8e182bb614e417c5958ed3d2aa
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-04-08 22:17:53 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,9 @@
+// 監聽安裝事件
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] 安裝成功');
+});
 
+// 監聽網頁請求 (Chrome 規定必須要有這個 fetch 監聽器，才會觸發安裝提示)
+self.addEventListener('fetch', (e) => {
+  // 這裡留空即可，我們暫時不需要複雜的離線暫存功能
+});
