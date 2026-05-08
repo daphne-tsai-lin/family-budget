a4a0f69139eddc0263fd8705be1f6ee5053cf91d
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 22:59:39 +0800

    Update useExpenses.js

--- src/hooks/useExpenses.js
+++ src/hooks/useExpenses.js
@@ -1,1 +1,30 @@
+import { useState, useEffect } from "react";
+import { getExpenses } from "../services/expenseService";
 
+export const useExpenses = () => {
+
+  const [expenses, setExpenses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const loadExpenses = async () => {
+
+    const data = await getExpenses();
+
+    setExpenses(data);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+
+    loadExpenses();
+
+  }, []);
+
+  return {
+    expenses,
+    loading,
+    reload: loadExpenses
+  };
+
+};
