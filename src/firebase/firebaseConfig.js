1ef78607ef4115c39b753b0a3077969ab8a06c58
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 23:17:04 +0800

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "你的key",
-  authDomain: "你的domain",
-  projectId: "你的projectId",
-  storageBucket: "你的storage",
-  messagingSenderId: "你的id",
-  appId: "你的appId"
+  apiKey: "AIzaSyBiFI05fIDz35Zk3n4nodHy9ZoYWqHOnZk",
+  authDomain: "lin-buget-7972c.firebaseapp.com",
+  projectId: "lin-buget-7972c",
+  storageBucket: "lin-buget-7972c.firebasestorage.app",
+  messagingSenderId: "451728257561",
+  appId: "1:451728257561:web:836705a26e6c879d10f088"
 };
 
 const app = initializeApp(firebaseConfig);
