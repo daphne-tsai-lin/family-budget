0a5967762917611d79ed5d874b3676a2d2d8af92
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 22:58:38 +0800

    Update expenseService.js

--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -1,1 +1,45 @@
+import { db } from "../firebase/firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  serverTimestamp
+} from "firebase/firestore";
 
+const expenseCollection = collection(db, "expenses");
+
+export const getExpenses = async () => {
+
+  const q = query(
+    expenseCollection,
+    orderBy("date", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data()
+  }));
+};
+
+export const addExpense = async (expense) => {
+
+  return await addDoc(expenseCollection, {
+    ...expense,
+    createdAt: serverTimestamp()
+  });
+
+};
+
+export const deleteExpense = async (id) => {
+
+  const ref = doc(db, "expenses", id);
+
+  await deleteDoc(ref);
+
+};
