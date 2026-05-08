4b8464cf5ac7d249ef344c93e9a8897a12340af4
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 23:00:37 +0800

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
 
+export default function ExpenseList({ expenses }) {
+
+  return (
+    <div>
+
+      {expenses.map(exp => (
+
+        <ExpenseItem
+          key={exp.id}
+          expense={exp}
+        />
+
+      ))}
+
+    </div>
+  );
+}
