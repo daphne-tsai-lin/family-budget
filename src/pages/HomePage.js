78694bbc172f33267acfba04909e9ff9a42c1511
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 23:03:43 +0800

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,1 +1,26 @@
+import ExpenseList from "../components/ExpenseList";
+import { useExpenses } from "../hooks/useExpenses";
 
+export default function HomePage() {
+
+  const { expenses, loading } = useExpenses();
+
+  if (loading) {
+
+    return <div>Loading...</div>;
+
+  }
+
+  return (
+
+    <div>
+
+      <h2>家庭記帳</h2>
+
+      <ExpenseList expenses={expenses} />
+
+    </div>
+
+  );
+
+}
