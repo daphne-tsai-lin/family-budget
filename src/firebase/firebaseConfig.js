909193ae33b8ac214d9b55ad88bad832fba76397
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-08 22:55:39 +0800

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "你的key",
+  authDomain: "你的domain",
+  projectId: "你的projectId",
+  storageBucket: "你的storage",
+  messagingSenderId: "你的id",
+  appId: "你的appId"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
