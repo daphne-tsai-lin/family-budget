03e9dccbcb237a16b8eb2d7bbc735e8cd0f54d1e
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-09 09:25:40 +0800

    Update AppProvider.js

--- src/app/AppProvider.js
+++ src/app/AppProvider.js
@@ -1,1 +1,9 @@
+import { AuthProvider } from "../store/AuthContext";
 
+export default function AppProvider({ children }) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+}
