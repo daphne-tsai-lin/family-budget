f7aad751f394692c778526a1403c479b755b5d9c
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-09 09:36:47 +0800

    Update AppRouter.js

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "../pages/DashboardPage";
 import ExpensePage from "../pages/ExpensePage";
 
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<DashboardPage />} />
-      <Route path="/expenses" element={<ExpensePage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/expenses" element={<ExpensePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
