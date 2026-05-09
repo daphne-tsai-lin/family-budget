32e43f8179b455779ac0cbad48a2efc7ff417903
daphne-tsai-lin <daphne.and.nn@gmail.com>
2026-05-09 09:37:58 +0800

    Update DashboardPage.js

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,1 +1,15 @@
+import { Link } from "react-router-dom";
 
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>記帳 App</h1>
+
+      <p>歡迎使用記帳系統</p>
+
+      <Link to="/expenses">
+        <button>進入記帳</button>
+      </Link>
+    </div>
+  );
+}
